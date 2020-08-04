da89742480e2a59c0c01df221172b56cc5d96a29
Felix Wolf <wolf@sonobeacon.com>
2020-08-04 12:42:23 +0200

    updated plugin to v1.2.0

--- Plugin/src/android/BootstrapApplication.java
+++ Plugin/src/android/BootstrapApplication.java
@@ -4,7 +4,10 @@ import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.util.Log;
 import com.sonobeacon.system.sonolib.SonoNet;
@@ -14,12 +17,30 @@ import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.service.RunningAverageRssiFilter;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class BootstrapApplication extends Application implements BootstrapNotifier {
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            fireEvent(intent.getAction().toString(), intent.getStringExtra("reminder_id"));
+        }
+    };
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("ENTER");
+        filter.addAction("EXIT");
+        filter.addAction("ble_enter");
+        filter.addAction("ble_exit");
+        registerReceiver(receiver, filter);
+
+
         BeaconManager beaconmanager = BeaconManager.getInstanceForApplication(this);
         beaconmanager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
         Notification.Builder builder = new Notification.Builder(this);
@@ -33,7 +54,7 @@ public class BootstrapApplication extends Application implements BootstrapNotifi
             notificationManager.createNotificationChannel(channel);
             builder.setChannelId(channel.getId());
         }
-        
+
         BeaconManager.setRssiFilterImplClass(RunningAverageRssiFilter.class);
         RunningAverageRssiFilter.setSampleExpirationMilliseconds(5000L);
         beaconmanager.enableForegroundServiceScanning(builder.build(),456);
@@ -41,23 +62,22 @@ public class BootstrapApplication extends Application implements BootstrapNotifi
         beaconmanager.setBackgroundBetweenScanPeriod(0);
         beaconmanager.setBackgroundScanPeriod(1100);
         Region region = new Region("region", null, null, null);
-
         new RegionBootstrap(this, region);
     }
 
-    @Override
-    public Context getApplicationContext() {
-        return super.getApplicationContext();
+    public void fireEvent(String enterAction, String id) {
+        SonoNet.Companion.regionEvent(this, enterAction, id);
     }
 
     @Override
     public void didEnterRegion(Region region) {
-        SonoNet.getInstance().sendNotification(region.getUniqueId());
+        SonoNet.Companion.regionEvent(this, "ENTER", region.getUniqueId());
         Log.d("RegionBootstrap", "didEnterRegion: " + region.getUniqueId());
     }
 
     @Override
     public void didExitRegion(Region region) {
+        SonoNet.Companion.regionEvent(this, "EXIT", region.getUniqueId());
         Log.d("RegionBootstrap", "didExitRegion: " + region.getUniqueId());
     }
 
